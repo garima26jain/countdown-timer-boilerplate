0ed63f9b2f23d36cb4ecd163ac2acb6aa898e525
garima <garimajain096@gmail.com>
2020-09-20 06:36:38 +0530

    first commit

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,34 +1,30 @@
-import React from 'react';
+import React from "react";
 
 class Clock extends React.Component {
-    formatTime(timeInSeconds) {
-        var seconds = timeInSeconds % 60;
-        var minutes = Math.floor(timeInSeconds / 60);
+  formatTime(timeInSeconds) {
+    var seconds = timeInSeconds % 60;
+    var minutes = Math.floor(timeInSeconds / 60);
 
-        if (seconds < 10) {
-            seconds = '0' + seconds;
-        }
-
-        if (minutes < 10) {
-            minutes = '0' + minutes;
-        }
-
-        return minutes + ':' + seconds;
+    if (seconds < 10) {
+      seconds = "0" + seconds;
     }
 
-    render() {
-        var {timeInSeconds} = this.props;
-        //Keep the classes name. Try to inject your code and do not remove existing code
-        return (
-            <div className="clock">
-                <span className="clock-text">
-                  
-                </span>
-            </div>
-        );
+    if (minutes < 10) {
+      minutes = "0" + minutes;
     }
+
+    return minutes + ":" + seconds;
+  }
+
+  render() {
+    var { timeInSeconds } = this.props;
+    //Keep the classes name. Try to inject your code and do not remove existing code
+    return (
+      <div className="clock">
+        <span className="clock-text">{this.formatTime(timeInSeconds)}</span>
+      </div>
+    );
+  }
 }
-
-
 
 export default Clock;
--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import Clock from './Clock';
-import CountdownForm from './CountdownForm';
+import React from "react";
+import Clock from "./Clock";
+import CountdownForm from "./CountdownForm";
 
 class Countdown extends React.Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+  changeState(counter) {
+    var tt = setInterval(() => {
+      this.setState({ count: counter-- });
+      if (counter < 0) {
+        clearInterval(tt);
+      }
+    }, 1000);
+  }
 
-    render() {
+  //   componentDidUpdate(prevState, prevProp) {
+  //     setInterval(() => {
+  //       if (prevState.count !== this.state.count) {
+  //         this.setState({ count: this.state.count - 1 });
+  //       }
+  //     }, 1000);
+  //   }
 
-        return (
-            <div>
-                <Clock timeInSeconds={count}/>
-                <CountdownForm onSetCountdownTime={}/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Clock timeInSeconds={this.state.count} />
+        <CountdownForm onSetCountdownTime={this.changeState.bind(this)} />
+      </div>
+    );
+  }
 }
 
 export default Countdown;
